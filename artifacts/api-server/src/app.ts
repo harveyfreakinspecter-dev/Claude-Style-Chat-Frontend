cc87a6b2158853efd53f87b6cee08ed924e538a1
harveyfreakinspecter-dev <harveyfreakinspecter@gmail.com>
2026-09-10 21:22:29 +0500

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,44 @@
+// import express, { type Express } from "express";
+// import cors from "cors";
+// //import pinoHttp from "pino-http";
+// import pinoHttp from "pino-http";
+// import type { Options } from "pino-http";
+// import router from "./routes";
+// import { logger } from "./lib/logger";
+
+// const app: Express = express();
+
+// app.use(
+//   pinoHttp({
+//     logger,
+//     serializers: {
+//       req(req) {
+//         return {
+//           id: req.id,
+//           method: req.method,
+//           url: req.url?.split("?")[0],
+//         };
+//       },
+//       res(res) {
+//         return {
+//           statusCode: res.statusCode,
+//         };
+//       },
+//     },
+//   }),
+// );
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api", router);
+
+// export default app;
+
+
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import { pinoHttp } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -10,14 +48,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: { id: string; method: string; url?: string }) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: { statusCode: number }) {
         return {
           statusCode: res.statusCode,
         };
